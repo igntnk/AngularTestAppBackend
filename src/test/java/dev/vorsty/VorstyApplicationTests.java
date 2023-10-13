bf6d0fb0aa6cdbf7735da67705f294111c8a81ad
igntnk <ighnatenko@vk.com>
2023-10-13 06:50:07 +0300

    add dockerfile

--- src/test/java/dev/vorsty/VorstyApplicationTests.java
+++ src/test/java/dev/vorsty/VorstyApplicationTests.java
@@ -1,13 +1,2 @@
 package dev.vorsty;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VorstyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
